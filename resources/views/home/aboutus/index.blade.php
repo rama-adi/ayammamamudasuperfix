5fd28a59f512e1c6cd9a040dc9eff0cc9c61711a
NessaW16 <146686207+NessaW16@users.noreply.github.com>
2023-11-11 01:13:43 +0700

    mek

--- resources/views/home/aboutus/index.blade.php
+++ resources/views/home/aboutus/index.blade.php
@@ -4,24 +4,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ayam Mama Muda - About</title>
-
-  <!-- CSS only -->
-   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-   <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-   <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-   <!-- fancybox -->
-   <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
-   <!-- Font Awesome 6 -->
-   <link rel="stylesheet" href="assets/css/fontawesome.min.css">
-   <!-- style -->
-   <link rel="stylesheet" href="assets/css/style.css">
-   <!-- responsive -->
-   <link rel="stylesheet" href="assets/css/responsive.css">
-   <!-- color -->
-   <link rel="stylesheet" href="assets/css/color.css">
-    <!-- jQuery -->
-   <script src="assets/js/jquery-3.6.0.min.js"></script>
-   <script src="assets/js/preloader.js"></script>
  </head>
 <body>
 
@@ -36,7 +18,7 @@
             </div>
             <ol class="breadcrumb">
                <li class="breadcrumb-item">
-                 <a href="/home"> Home</a>
+                 <a href="/contactus"> Contact Us</a>
 
             </ol>
          </div>
