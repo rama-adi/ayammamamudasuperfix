fd69511d50b0951bb708adc673692cc0ad9a5f39
NessaW16 <146686207+NessaW16@users.noreply.github.com>
2023-11-12 20:00:12 +0700

    a

--- resources/views/home/aboutus/index.blade.php
+++ resources/views/home/aboutus/index.blade.php
@@ -49,69 +49,6 @@
 </section>
 
 
-<section class="gap no-bottom">
-    <div class="container">
-       <div class="heading-two">
-          <h2>Meet Our Team</h2>
-          <div class="line"></div>
-       </div>
-       <div class="row">
-          <div class="col-xl-4 col-lg-6">
-             <div class="chef">
-                <img alt="imgED" src="{{ asset('img/edbert.png') }}">
-                <div class="chef-text">
-                   <div>
-                      <span>Founder</span>
-                      <a href="chef-details.html"><h3>Enrico Puwarta</h3></a>
-                      <ul class="social-media">
-                         <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                         <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                         <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                      </ul>
-
-                   </div>
-                </div>
-             </div>
-          </div>
-          <div class="col-xl-4 col-lg-6">
-             <div class="chef">
-                <img alt="imgG" src="{{ asset('img/edbert.png') }}">
-                <div class="chef-text">
-                   <div>
-                      <span>CEO</span>
-                      <a href="chef-details.html"><h3>Edbert</h3></a>
-                      <ul class="social-media">
-                         <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                         <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                         <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                      </ul>
-
-                   </div>
-                </div>
-             </div>
-          </div>
-          <div class="col-xl-4 col-lg-6">
-             <div class="chef mb-0">
-                <img alt="imgED" src="{{ asset('img/edbert.png') }}">
-                <div class="chef-text">
-                   <div>
-                      <span>Co Founder</span>
-                      <a href="chef-details.html"><h3>Rian Surata</h3></a>
-                      <ul class="social-media">
-                         <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                         <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                         <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                      </ul>
-
-                   </div>
-                </div>
-             </div>
-          </div>
-       </div>
-    </div>
- </section>
-
-
 <section class="gap" style="background-color:#f5f8fd;">
    <div class="container">
       <div class="heading-two">
